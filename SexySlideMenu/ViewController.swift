cc2e5db193c73a65b7fe3326a7e130d2166d0196
yoontaesup <rupy1014@gmail.com>
2015-06-24 10:54:48 +0900

    Initial Commit

---
+++ SexySlideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SexySlideMenu
+//
+//  Created by YoonTaesup on 2015. 6. 24..
+//  Copyright (c) 2015년 com.SexyDynamite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
